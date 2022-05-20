c28668951e3aec10b96c8058f37980e058b42151
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-05-20 15:32:25 +0300

    Add GameController

---
+++ src/main/java/spellingbee/controllers/GameController.java
@@ -0,0 +1,11 @@
+package spellingbee.controllers;
+
+import spellingbee.models.GameModel;
+
+public class GameController {
+    private GameModel model;
+
+    public GameController(GameModel model){
+        this.model=model;
+    }
+}
