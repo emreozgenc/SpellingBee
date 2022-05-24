f2db8e8d1feb8ec9d9d148624d31de037f3f3809
emreozgenc <emreozgenc@hotmail.com.tr>
2022-05-24 23:43:33 +0300

    Fix typo

--- src/main/java/spellingbee/core/constants/UINames.java
+++ src/main/java/spellingbee/core/constants/UINames.java
@@ -6,7 +6,7 @@ public class UINames {
     public static final String DELETE_BUTTON = "Sil";
     public static final String POINT_LABEL = "Puan : ";
     public static final String START_BUTTON = "Oyunu Başlat";
-    public static final String EXIT_BUTTON = "Çıkış";
+    public static final String EXIT_BUTTON = "Çıkış Yap";
     public static final String MENU_FIELD_PROMPT = "Kullanılacak harfleri giriniz...";
 
 }
