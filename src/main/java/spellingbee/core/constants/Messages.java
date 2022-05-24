1125e33518cc95d44d34896dd7e637cf05e78a52
nrllahh <atasnurullahh@gmail.com>
2022-05-24 13:59:05 +0300

    Add Messages

---
+++ src/main/java/spellingbee/core/constants/Messages.java
@@ -0,0 +1,13 @@
+package spellingbee.core.constants;
+
+public class Messages {
+    public static final String ILLEGAL_POINT_RANGE = "İstenmeyen puan aralığı!";
+    public static final String PANGRAM_NOT_FOUND = "Pangram kelime bulunamadı";
+    public static final String NOT_ENOUGH_WORDS = "Yeterli sayıda kelime bulunamadı";
+    public static final String NOT_UNIQUE_LETTERS = "Harfler benzersiz değil";
+    public static final String ILLEGAL_LETTERS_LENGTH = "Harfler 7 karakterden uzun veya kısa";
+    public static final String DICTIONARY_DOES_NOT_CONTAIN_WORD = "Kelime sözlükte bulunamadı";
+    public static final String WORD_DOES_NOT_CONTAIN_CENTER_LETTER = "Kelime merkez harfi içermiyor";
+    public static final String ILLEGAL_WORD_LENGTH = "Kelime 4 karakterden kısa";
+    public static final String WORD_CONTAINS_ILLEGAL_LETTER = "Kelime uygunsuz harf içeriyor";
+}
