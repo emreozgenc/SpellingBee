e74a5c39122aa780b35c4a349c7565ac7cb46d1f
nrllahh <atasnurullahh@gmail.com>
2022-05-24 13:18:14 +0300

    Add missing exception constructors

--- src/main/java/spellingbee/core/exceptions/WordContainsIllegalLetterException.java
+++ src/main/java/spellingbee/core/exceptions/WordContainsIllegalLetterException.java
@@ -1,4 +1,11 @@
 package spellingbee.core.exceptions;
 
 public class WordContainsIllegalLetterException extends Exception {
+    public WordContainsIllegalLetterException() {
+        super();
+    }
+
+    public WordContainsIllegalLetterException(String message) {
+        super(message);
+    }
 }
--- src/main/java/spellingbee/core/exceptions/WordDoesNotContainCenterLetterException.java
+++ src/main/java/spellingbee/core/exceptions/WordDoesNotContainCenterLetterException.java
@@ -1,4 +1,11 @@
 package spellingbee.core.exceptions;
 
 public class WordDoesNotContainCenterLetterException extends Exception {
+    public WordDoesNotContainCenterLetterException() {
+        super();
+    }
+
+    public WordDoesNotContainCenterLetterException(String message) {
+        super(message);
+    }
 }
