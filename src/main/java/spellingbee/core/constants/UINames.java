720952e55ff5a282872670ab4571d14d155a30c7
emreozgenc <emreozgenc@hotmail.com.tr>
2022-05-24 23:32:56 +0300

    Remove unused constants and Add new ones

--- src/main/java/spellingbee/core/constants/UINames.java
+++ src/main/java/spellingbee/core/constants/UINames.java
@@ -5,7 +5,8 @@ public class UINames {
     public static final String ENTER_BUTTON = "Onayla";
     public static final String DELETE_BUTTON = "Sil";
     public static final String POINT_LABEL = "Puan : ";
-    public static final String START_BUTTON = "Başlat";
-    public static final String CREATE_BUTTON = "Oluştur";
+    public static final String START_BUTTON = "Oyunu Başlat";
+    public static final String EXIT_BUTTON = "Çıkış";
+    public static final String MENU_FIELD_PROMPT = "Kullanılacak harfleri giriniz...";
 
 }
