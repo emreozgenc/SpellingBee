76ec1db46762d84a63687ff137139086903b5f3c
emreozgenc <emreozgenc@hotmail.com.tr>
2022-05-25 21:39:58 +0300

    Add shuffle method and BeehiveCell constants, constructor changes, format code

--- src/main/java/spellingbee/components/Beehive.java
+++ src/main/java/spellingbee/components/Beehive.java
@@ -1,13 +1,19 @@
 package spellingbee.components;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+import java.util.Random;
 
 public class Beehive extends Pane {
 
     // Temporary
-    private String[] values = {"A", "B", "C", "D", "E", "F", "G"};
+    private String letters;
     private BeehiveCell[] cells;
     private final double CELL_EDGE_WIDTH = 50;
+    private final Color CENTER_CELL_BACKGROUND_OUTPUT = Color.rgb(255, 209, 0);
+    private final Color CELL_OUTPUT = Color.BLACK;
+    private final Color CELL_BACKGROUND = Color.rgb(225, 225, 225);
     private final int[][] cellMap = {
             {0, 1, 0},
             {1, 0, 1},
@@ -16,24 +22,38 @@ public class Beehive extends Pane {
             {0, 1, 0}
     };
 
-    public Beehive() {
-        int polygonCount = 0;
-        cells = new BeehiveCell[7];
-        for(int i=0;i<cells.length;i++) {
-            cells[i] = new BeehiveCell(values[i], CELL_EDGE_WIDTH);
+    public Beehive(String letters) {
+        this.letters = letters;
+        cells = new BeehiveCell[letters.length()];
+        for (int i = 0; i < cells.length; i++) {
+            cells[i] = new BeehiveCell(Character.toString(letters.charAt(i)),
+                    CELL_EDGE_WIDTH,
+                    i == cells.length / 2 ? CENTER_CELL_BACKGROUND_OUTPUT : CELL_BACKGROUND,
+                    i == cells.length / 2 ? CENTER_CELL_BACKGROUND_OUTPUT : CELL_OUTPUT);
         }
         int cellIndex = 0;
-        for(int i=0;i<cellMap.length;i++) {
-            for(int j=0;j<cellMap[i].length;j++) {
-                if(cellMap[i][j] == 1) {
+        for (int i = 0; i < cellMap.length; i++) {
+            for (int j = 0; j < cellMap[i].length; j++) {
+                if (cellMap[i][j] == 1) {
                     BeehiveCell cell = cells[cellIndex];
-                    cell.setLayoutX(j*1.5*CELL_EDGE_WIDTH);
-                    cell.setLayoutY(i*Math.sqrt(3)*0.5*CELL_EDGE_WIDTH);
+                    cell.setLayoutX(j * 1.5 * CELL_EDGE_WIDTH);
+                    cell.setLayoutY(i * Math.sqrt(3) * 0.5 * CELL_EDGE_WIDTH);
                     cellIndex++;
                 }
             }
         }
-
         getChildren().addAll(cells);
+    }
+
+    public void shuffle() {
+        Random random = new Random();
+        for (int i = 0; i < cells.length; i++) {
+            int randomIndex = random.nextInt(cells.length);
+            if (randomIndex == cells.length / 2 || i == cells.length / 2) continue;
+
+            String temp = cells[i].getCellValue();
+            cells[i].setCellValue(cells[randomIndex].getCellValue());
+            cells[randomIndex].setCellValue(temp);
+        }
     }
 }
