42e8c95795b2090ef7fc74d6c92f3694a51a1df1
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-05-27 16:30:05 +0300

    Change check method's return type to PointResult

--- src/main/java/spellingbee/core/managers/GameService.java
+++ src/main/java/spellingbee/core/managers/GameService.java
@@ -4,7 +4,8 @@ import spellingbee.core.exceptions.DictionaryDoesNotContainWordException;
 import spellingbee.core.exceptions.IllegalWordLengthException;
 import spellingbee.core.exceptions.WordContainsIllegalLetterException;
 import spellingbee.core.exceptions.WordDoesNotContainCenterLetterException;
+import spellingbee.core.results.PointResult;
 
 public interface GameService {
-    int check(String inputWord) throws DictionaryDoesNotContainWordException, IllegalWordLengthException, WordContainsIllegalLetterException, WordDoesNotContainCenterLetterException;
+    PointResult check(String inputWord) throws DictionaryDoesNotContainWordException, IllegalWordLengthException, WordContainsIllegalLetterException, WordDoesNotContainCenterLetterException;
 }
