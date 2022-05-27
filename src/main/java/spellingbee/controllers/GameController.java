bcfbaf65e3e39b26a8b4ef46b35a68e1607ebcf1
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-05-27 15:44:00 +0300

    Add GameService in constructor

--- src/main/java/spellingbee/controllers/GameController.java
+++ src/main/java/spellingbee/controllers/GameController.java
@@ -1,11 +1,17 @@
 package spellingbee.controllers;
 
+import spellingbee.core.exceptions.DictionaryDoesNotContainWordException;
+import spellingbee.core.exceptions.IllegalWordLengthException;
+import spellingbee.core.exceptions.WordContainsIllegalLetterException;
+import spellingbee.core.exceptions.WordDoesNotContainCenterLetterException;
+import spellingbee.core.managers.GameService;
 import spellingbee.models.GameModel;
 
 public class GameController {
     private GameModel model;
-
-    public GameController(GameModel model){
+    private GameService gameService;
+    public GameController(GameModel model, GameService gameService){
         this.model=model;
+        this.gameService=gameService;
     }
 }
