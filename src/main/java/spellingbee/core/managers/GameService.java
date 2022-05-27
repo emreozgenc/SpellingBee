0bc0d1fd617e8be649d7dfd30e9f6d7db3b8a6a0
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-05-27 18:57:32 +0300

    Fix word already found problem

--- src/main/java/spellingbee/core/managers/GameService.java
+++ src/main/java/spellingbee/core/managers/GameService.java
@@ -1,11 +1,8 @@
 package spellingbee.core.managers;
 
-import spellingbee.core.exceptions.DictionaryDoesNotContainWordException;
-import spellingbee.core.exceptions.IllegalWordLengthException;
-import spellingbee.core.exceptions.WordContainsIllegalLetterException;
-import spellingbee.core.exceptions.WordDoesNotContainCenterLetterException;
+import spellingbee.core.exceptions.*;
 import spellingbee.core.results.PointResult;
 
 public interface GameService {
-    PointResult check(String inputWord) throws DictionaryDoesNotContainWordException, IllegalWordLengthException, WordContainsIllegalLetterException, WordDoesNotContainCenterLetterException;
+    PointResult check(String inputWord) throws DictionaryDoesNotContainWordException, IllegalWordLengthException, WordContainsIllegalLetterException, WordDoesNotContainCenterLetterException, WordAlreadyFoundException;
 }
