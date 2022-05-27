1c58b8d59a914ff459589ca1090b0ac412de9779
emreozgenc <emreozgenc@hotmail.com.tr>
2022-05-27 15:35:23 +0300

    Add Colors contants

---
+++ src/main/java/spellingbee/core/constants/Colors.java
@@ -0,0 +1,12 @@
+package spellingbee.core.constants;
+
+import javafx.scene.paint.Color;
+
+public class Colors {
+    public static final Color CELL_POLYGON = Color.LIGHTGRAY;
+    public static final Color CELL_OUTPUT = Color.BLACK;
+    public static final Color CELL_CENTER_POLYGON = Color.rgb(255, 209, 0);
+    public static final Color CELL_CENTER_OUTPUT = Color.rgb(255, 209, 0);
+    public static final Color MENU_BACKGROUND = Color.rgb(255, 209, 0);
+    public static final Color WRONG_OUTPUT = Color.LIGHTGRAY;
+}
