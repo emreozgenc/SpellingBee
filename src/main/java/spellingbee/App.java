779b0fc6842212eb76fa80818769d8bac0408da0
emreozgenc <emreozgenc@hotmail.com.tr>
2022-05-29 22:00:09 +0300

    Remove unused imports, optimization changes ( DataReader and DataFilter initialization early stage )

--- src/main/java/spellingbee/App.java
+++ src/main/java/spellingbee/App.java
@@ -1,49 +1,45 @@
 package spellingbee;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import spellingbee.controllers.GameController;
 import spellingbee.controllers.MenuController;
+import spellingbee.core.data.DataFilter;
 import spellingbee.core.data.DataReader;
 import spellingbee.core.data.TxtReader;
 import spellingbee.core.data.WordFilter;
-import spellingbee.models.GameModel;
 import spellingbee.models.MenuModel;
-import spellingbee.views.GameView;
 import spellingbee.views.MenuView;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Scanner;
-
 
 public class App extends Application {
 
     private static App instance = null;
     private Stage stage = null;
+    private DataReader dataReader;
+    private DataFilter dataFilter;
 
     @Override
     public void init() {
         instance = this;
+        dataReader = new TxtReader();
+        dataFilter = new WordFilter();
+        dataReader.read();
     }
 
     @Override
     public void start(Stage stage) {
         this.stage = stage;
+
         MenuModel model = new MenuModel();
-        MenuController controller = new MenuController(model, new TxtReader(), new WordFilter());
+        MenuController controller = new MenuController(model, dataReader, dataFilter);
         MenuView view = new MenuView(model, controller);
         Scene scene = new Scene(view.getAsParent());
         scene.getStylesheets().addAll("style.css");
-        stage.setTitle("Spelling Bee v0.0.1");
+
+        stage.setTitle("Arı Kovanı v1.0.0");
+        stage.getIcons().add(new Image("logo.png"));
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
@@ -58,6 +54,14 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public DataReader getDataReader() {
+        return dataReader;
+    }
+
+    public DataFilter getDataFilter() {
+        return dataFilter;
     }
 
 }
