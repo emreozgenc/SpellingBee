3309ca119772fe9edfa9ef7d912c62b9152d069a
emreozgenc <emreozgenc@hotmail.com.tr>
2022-05-29 21:43:53 +0300

    Optimization changes and Add replaceCharacters methods to fix ( â != a ) problem

--- src/main/java/spellingbee/core/data/TxtReader.java
+++ src/main/java/spellingbee/core/data/TxtReader.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class TxtReader implements DataReader {
     private final String FILE_PATH = "data.txt";
     private File file;
+    private List<String> data;
 
     public TxtReader() {
         file = new File(getClass().getClassLoader().getResource(FILE_PATH).getFile());
@@ -18,17 +19,31 @@ public class TxtReader implements DataReader {
     public List<String> read() {
         List<String> words = new ArrayList<>();
         try {
+            if (data != null) return data;
+
             Scanner scanner = new Scanner(file, StandardCharsets.UTF_8);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                if(line.length() > 3)
-                    words.add(line.toLowerCase());
+                if (line.length() > 3) {
+                    line = replaceCharacters(line);
+                    words.add(line);
+                }
             }
+
         } catch (Exception exception) {
             // To do
         }
 
-        return words;
+        return data = words;
+    }
+
+    private String replaceCharacters(String str) {
+        str = str.toLowerCase();
+        str = str.replaceAll("â", "a");
+        str = str.replaceAll("û", "u");
+        str = str.replaceAll("î", "ı");
+
+        return str;
     }
 
 }
