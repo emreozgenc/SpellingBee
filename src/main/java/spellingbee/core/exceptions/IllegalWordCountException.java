3169baff49b40ccc7bed5ba39a51db93318212fd
Emre Özgenç <emreozgenc@hotmail.com.tr>
2022-06-08 22:12:11 +0300

    Change NotEnoughWordsException name to IllegalWordCountException

---
+++ src/main/java/spellingbee/core/exceptions/IllegalWordCountException.java
@@ -0,0 +1,11 @@
+package spellingbee.core.exceptions;
+
+public class IllegalWordCountException extends Exception {
+    public IllegalWordCountException() {
+        super();
+    }
+
+    public IllegalWordCountException(String message) {
+        super(message);
+    }
+}
